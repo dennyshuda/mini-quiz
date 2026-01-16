6746c699d19ded708b1cceb1bb8ca8f5eb62c5b8
Denny Setyawan Huda <denycam9@gmail.com>
2026-01-16 18:58:47 +0700

    feat: update routing configuration to include quiz and authentication routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,16 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+	layout("./quiz/_layout.tsx", [
+		index("./quiz/home.tsx"),
+		...prefix("quiz", [
+			route("history", "./quiz/history.tsx"),
+			route("attempt/:id", "./quiz/attempt.tsx"),
+		]),
+	]),
+
+	route("register", "./auth/register.tsx"),
+	route("login", "./auth/login.tsx"),
+	route("verify", "./auth/verify.tsx"),
+	route("@me", "./auth/me.tsx"),
+] satisfies RouteConfig;
