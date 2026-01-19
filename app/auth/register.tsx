b82580b19588dbc8e620872ee845a84be3ebea72
Denny Setyawan Huda <denycam9@gmail.com>
2026-01-19 19:07:25 +0700

    feat: implement registration logic with session handling and form submission

--- app/auth/register.tsx
+++ app/auth/register.tsx
@@ -1,15 +1,41 @@
 import { IconAlertCircle, IconLoader, IconLock, IconMail, IconUser } from "@tabler/icons-react";
-import { registerAction } from "actions/auth";
 import { Input } from "components/input";
-import { useActionState } from "react";
-import { Link } from "react-router";
+import { axiosInstance } from "lib/axios";
+import { Form, Link, redirect, useNavigation } from "react-router";
+import { handleApiError } from "utils/error-handler";
+import { getSession } from "~/session.server";
+import type { Route } from "./+types/register";
 
-export default function RegisterPage() {
-	const [state, formAction, isPending] = useActionState(registerAction, {
-		success: false,
-		message: null,
-		code: undefined,
-	});
+export async function loader({ request }: Route.LoaderArgs) {
+	const session = await getSession(request.headers.get("Cookie"));
+
+	if (session.has("access_token")) {
+		return redirect("/secure");
+	}
+}
+
+export async function action({ request }: Route.ActionArgs) {
+	const session = await getSession(request.headers.get("Cookie"));
+
+	const form = await request.formData();
+
+	const name = form.get("name") as string;
+	const email = form.get("email") as string;
+	const password = form.get("password") as string;
+
+	const axios = axiosInstance(session.get("access_token"));
+
+	try {
+		const response = await axios.post("/auth/register", { name, email, password });
+		return redirect("/login");
+	} catch (err: any) {
+		return handleApiError(err);
+	}
+}
+
+export default function RegisterPage({ actionData }: Route.ComponentProps) {
+	const navigation = useNavigation();
+	const isSubmitting = navigation.state === "submitting";
 
 	return (
 		<div className="min-h-screen bg-[#F8FAFC] flex items-center justify-center p-6">
@@ -19,14 +45,14 @@ export default function RegisterPage() {
 					<p className="text-slate-500 mt-2 font-medium">Mulai perjalanan ambisiusmu sekarang.</p>
 				</div>
 
-				{state.message && !state.success && (
+				{actionData?.message && !actionData.success && (
 					<div className="flex items-center gap-2 p-4 mb-6 bg-red-50 border border-red-100 text-red-600 rounded-2xl animate-shake">
 						<IconAlertCircle size={18} className="shrink-0" />
-						<span className="text-sm font-bold">{state.message}</span>
+						<span className="text-sm font-bold">{actionData.message}</span>
 					</div>
 				)}
 
-				<form action={formAction} className="space-y-6">
+				<Form method="POST" className="space-y-6">
 					<Input
 						label="Nama Lengkap"
 						name="name"
@@ -55,12 +81,13 @@ export default function RegisterPage() {
 
 					<button
 						type="submit"
-						disabled={isPending}
+						disabled={isSubmitting}
 						className="w-full cursor-pointer bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-4 rounded-2xl flex items-center justify-center gap-2 shadow-lg shadow-indigo-100 transition-all active:scale-95"
 					>
-						{isPending ? <IconLoader className="size-5" /> : <IconLock className="size-5" />} Daftar
+						{isSubmitting ? <IconLoader className="size-5" /> : <IconLock className="size-5" />}{" "}
+						Daftar
 					</button>
-				</form>
+				</Form>
 
 				<p className="mt-8 text-center text-slate-500 font-medium text-sm">
 					Sudah punya akun?{" "}
